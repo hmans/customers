99561026dfb49df368a45b8f35c264c2f5595c2f
Hendrik Mans <hendrik@mans.de>
2012-09-24 20:44:40 +0200

    Add some validations to the customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :birthday, :email, :first_name, :last_name
+
+  validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email
 end
