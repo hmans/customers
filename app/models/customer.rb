37bd88e982c589f02a6cc37832ac13e181781da0
Hendrik Mans <hendrik@mans.de>
2012-09-25 15:02:56 +0200

    Add customer specs

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
+
+  def full_name
+    if last_name.present? && first_name.present?
+      "#{last_name}, #{first_name}"
+    else
+      ""
+    end
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Customer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @customer = Customer.new
+  end
+
+  describe '#full_name' do
+    context "without first_name and last_name" do
+      it "should return an empty string" do
+        @customer.full_name.should == ""
+      end
+    end
+
+    context "with first_name and last_name" do
+      before do
+        @customer.first_name = "Manfred"
+        @customer.last_name = "Mustermann"
+      end
+
+      it "should return the full name" do
+        @customer.full_name.should == "#{@customer.last_name}, #{@customer.first_name}"
+      end
+    end
+  end
 end
