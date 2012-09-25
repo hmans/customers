dda2734d56aa5b350df0ce7cddbeaaa7dc9be392
Hendrik Mans <hendrik@mans.de>
2012-09-25 15:04:22 +0200

    Use 'its' shortcut

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,22 +5,24 @@ describe Customer do
     Customer.new
   end
 
-  describe '#full_name' do
-    context "when the customer has no first_name and last_name" do
-      it "should return an empty string" do
-        subject.full_name.should == ""
-      end
+  context "when the customer has no first_name and last_name" do
+    # it "should have a full_name method returning an empty string" do
+    #   subject.full_name.should == ""
+    # end
+
+    its(:full_name) { should == "" }
+  end
+
+  context "when the customer has first_name and last_name" do
+    before do
+      subject.first_name = "Manfred"
+      subject.last_name = "Mustermann"
     end
 
-    context "when the curstomer has first_name and last_name" do
-      before do
-        subject.first_name = "Manfred"
-        subject.last_name = "Mustermann"
-      end
-
-      it "should return the full name" do
-        subject.full_name.should == "#{subject.last_name}, #{subject.first_name}"
-      end
+    it "should have a full_name method returning the full name" do
+      subject.full_name.should == "#{subject.last_name}, #{subject.first_name}"
     end
+
+    # its(:full_name) { should == "#{subject.last_name}, #{subject.first_name}" }
   end
 end
